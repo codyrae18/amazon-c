f89164a263e5f49fe529e922830a5f83ed70a11c
codyrae18 <lawdermiltcody@gmai.com>
2021-04-09 23:54:00 -0500

    created home components and just about to start to built it

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function header() {
   return (
@@ -29,9 +30,9 @@ function header() {
           <span className="header-optionLine1">Your</span>
           <span className="header-optionLine2">Prime</span>
         </div>
-        <div className="header-option">
-          <span className="header-optionLine1">Hello Guest</span>
-          <span className="header-optionLine2">Sign in</span>
+        <div className="header-optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header-optionLine2 header-basetCount">0</span>
         </div>
       </div>
     </div>
